424ec38cc80e2cc99b616113b418a3a0aab7d29c
Abhishek Malakar <45138938+Abhishekmalakar@users.noreply.github.com>
2022-12-25 11:47:40 +0530

    App Components
    This is the App Component/Main Component which can be generated by angular

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+
+  title = 'astoriaTrainingAngular';
+  constructor(){
+
+  }
+
+
+
+  }
+
+
